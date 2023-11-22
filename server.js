79ee2637ad176ed156967386f98c8581285defa8
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2023-11-22 21:28:11 +0930

    adjust some files

--- server.js
+++ server.js
@@ -4,31 +4,17 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 
-//define middleware function
-// const mw = (req, res, next) => {
-//   console.log("This is a mw function");
-//   next();
-// };
-
 //register global middleware
 app.use(express.static("."));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors()); //必须当成全局中间件使用
-// app.use(mw);
-// app.use((req, res, next) => {
-//   console.log("This is another mw function");
-//   next();
-// });
 
 // require router module
 const userListRouter = require("./router");
 
 // use router module
 app.use("/api", userListRouter);
-// app.get("/api/home", (req, res) => {
-//   res.send("This is home page");
-// });
 
 // start web server
 const PORT = 50006;
